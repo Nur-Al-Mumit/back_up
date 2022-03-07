3d2499b3820f5ebf948348ee16a21af5fbf852a8
nuralmumit <nuralmumit@gmail.com>
2022-03-07 19:30:31 +0600

    make the Website responsive

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ const displayItems = (inputData) => {
   phoneDetails.textContent = "";
   inputData.forEach((inputDatas) => {
     const div = document.createElement("div");
-    div.classList.add("col");
+    // div.classList.add("grid grid-cols-3");
     div.innerHTML = `
-      <div class="card">
-                <img src=${inputDatas.image} class="card-img-top w-50"/>
-                <div class="card-body">
-                  <h5 class="card-title">${inputDatas.phone_name}</h5>
-                  <p class="card-text">
+      <div>
+                <img src=${inputDatas.image} class=""/>
+                <div class="w-full">
+                  <h5 class="w-full">${inputDatas.phone_name}</h5>
+                  <p class="w-full">
                     This is a longer card with supporting text below as a natural
                     lead-in to additional content. This content is a little bit
                     longer.
                   </p>
-                  <button  onclick="allPhoneDetails('${inputDatas.slug}')" type="button" class="btn btn-outline-secondary">Details</button>
+                  <button  onclick="allPhoneDetails('${inputDatas.slug}')" type="button" class="bg-violet-600 hover:bg-violet-700 text-white font-bold py-2 px-5 rounded-full">Details</button>
                 </div>
               </div>
               `;
@@ -49,19 +49,20 @@ const allPhoneDetails = (detailsId) => {
 };
 const showPhoneDetails = (allDetails) => {
   const detailsDiv = document.createElement("div");
+  // detailsDiv.classList.add("w-full");
   phoneDetails.textContent = "";
   detailsDiv.innerHTML = `
-    <div class="card mx-auto w-50 d-flex flex-row mb-4">
-    <img src="${allDetails.data.image}" class="card-img-top" alt="">
-    <div class="card-body">
-    <h1 class="card-title">${allDetails.data.name}</h1>
-    <p class="card-text">- ${allDetails.data.release}</p>;
-    <p class="card-text">- ${allDetails.data.mainFeatures.storage}</p>
-    <p class="card-text">- ${allDetails.data.mainFeatures.displaySize}</p>
-    <p class="card-text">- ${allDetails.data.mainFeatures.chipSet}</p>
-    <p class="card-text">- ${allDetails.data.mainFeatures.memory}</p>
-    <p class="card-text">- ${allDetails.data.mainFeatures.sensors}</p>
-    <a href="#" class="btn btn-primary">Buy Now</a>
+    <div class="flex flex-col md:flex-row lg:flex-row justify-center lg:justify-around md:justify-around sm:justify-around">
+    <div><img src="${allDetails.data.image}"></div>
+    <div class="w-full">
+    <h1>${allDetails.data.name}</h1>
+    <p>- ${allDetails.data.releaseDate}</p>;
+    <p>- ${allDetails.data.mainFeatures.storage}</p>
+    <p>- ${allDetails.data.mainFeatures.displaySize}</p>
+    <p>- ${allDetails.data.mainFeatures.chipSet}</p>
+    <p>- ${allDetails.data.mainFeatures.memory}</p>
+    <p class="mb-4">- ${allDetails.data.mainFeatures.sensors}</p>
+    <a href="#" class="bg-violet-600 hover:bg-violet-700 text-white font-bold py-2 px-5 rounded-full w-full">Buy Now</a>
     </div>
   </div>
   `;
