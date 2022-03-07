ab32653ad0e363b066f1f708dec17e7c55018b3d
nuralmumit <nuralmumit@gmail.com>
2022-03-07 21:57:11 +0600

    add the error condition

--- script.js
+++ script.js
@@ -8,36 +8,40 @@ const inputValue = () => {
     inputAria.value = "";
     const url = ` https://openapi.programming-hero.com/api/phones?search=${inputText}`;
     fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayItems(data.data));
+      .then((res) => res.json())
+      .then((data) => displayItems(data.data));
   }
 };
 // Show the Fetch data on UI
 const phoneDetails = document.getElementById("phone-details");
 const displayItems = (inputData) => {
-  const display = document.getElementById("display-container");
-  display.textContent = "";
-  phoneDetails.textContent = "";
-  inputData.forEach((inputDatas) => {
-    const div = document.createElement("div");
-    // div.classList.add("grid grid-cols-3");
-    div.innerHTML = `
-      <div>
-                <img src=${inputDatas.image} class=""/>
-                <div class="w-full">
-                  <h2 class="text-3xl">${inputDatas.phone_name}</h2>
-                  <p>${inputDatas.brand}</p>
-                  <p class="w-full">
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <button  onclick="allPhoneDetails('${inputDatas.slug}')" type="button" class="bg-violet-600 hover:bg-violet-700 text-white font-bold py-2 px-5 rounded-full">Details</button>
-                </div>
-              </div>
-              `;
-    display.appendChild(div);
-  });
+  if (inputData.length == 0) {
+    alert("We can't find any phone");
+  } else {
+    const display = document.getElementById("display-container");
+    display.textContent = "";
+    phoneDetails.textContent = "";
+    inputData.forEach((inputDatas) => {
+      const div = document.createElement("div");
+      // div.classList.add("grid grid-cols-3");
+      div.innerHTML = `
+        <div>
+        <img src=${inputDatas.image} class=""/>
+        <div class="w-full">
+        <h2 class="text-3xl">${inputDatas.phone_name}</h2>
+        <p>${inputDatas.brand}</p>
+        <p class="w-full">
+        This is a longer card with supporting text below as a natural
+        lead-in to additional content. This content is a little bit
+        longer.
+        </p>
+        <button  onclick="allPhoneDetails('${inputDatas.slug}')" type="button" class="bg-violet-600 hover:bg-violet-700 text-white font-bold py-2 px-5 rounded-full">Details</button>
+        </div>
+        </div>
+        `;
+      display.appendChild(div);
+    });
+  }
 };
 // To make the Details button dynamic and show phone's Details on UI
 
